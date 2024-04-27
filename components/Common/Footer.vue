21f88de58f6a2733284fde5f6f16aa1614a8f587
atrillowvn <163084340+atrillowvn@users.noreply.github.com>
2024-04-27 09:56:00 +0800

    Update Footer.vue

--- components/Common/Footer.vue
+++ components/Common/Footer.vue
@@ -6,10 +6,10 @@
 				<span
 					>Powered by
 					<a
-						href="https://www.fre123.com"
+						href="https://www.redditfans.com"
 						target="_blank"
 						class="decoration-transparent text-[#a4715c]"
-						>FRE123</a
+						>RedditFans</a
 					>
 				</span>
 			</div>
